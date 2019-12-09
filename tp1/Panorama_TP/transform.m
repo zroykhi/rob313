8748252420885ae3db896789449be0c3e0d65a2f
zroykhi <727125137@qq.com>
2019-12-09 23:16:39 +0100

    Q2 done

--- tp1/Panorama_TP/transform.m
+++ tp1/Panorama_TP/transform.m
@@ -1,3 +1,4 @@
+% Q1
 imrgb = double(imread('Pompei.jpg'))/255;
 imagesc(imrgb);
 
@@ -6,24 +7,23 @@ PtD = [120, 20, 1; 320, 20, 1; 120, 320, 1; 320, 320, 1]'
 H = homography2d(PtO,PtD);
 imagesc(vgg_warp_H(imrgb,H))
 
-
+% Q2.1
 PtO = [404.08, 154.16, 1;424.04, 154.16, 1;404.08, 186.18, 1;424.04, 186.18, 1]';
 PtD = [22.609, 152.03, 1;43.007, 152.03, 1;22.609, 184.82, 1;43.007, 184.82, 1]';
 H1 = homography2d(PtO,PtD);
-bbox = [-1000 500 -100 500];
+% bbox = [-1000 500 -100 500];
+bbox = [-1500 500 -100 500];
 img = double(imread('Amst-2.jpg'))/255;
 ima=vgg_warp_H(img,H1,'linear',bbox);
 imagesc(ima);
-
 H2 = eye(3);
 img = double(imread('Amst-3.jpg'))/255;
 imb=vgg_warp_H(img,H2,'linear',bbox);
 imagesc(imb);
-
-
 im_fused = max(ima,imb); % find new coodinate of Amst-2 as PtD
 imagesc(im_fused);
 
+% Q2.2
 img3 = double(imread('Amst-1.jpg'))/255;
 % PtO = [472.18, 135.74, 1;486.42, 139.97, 1;472.18, 146.31, 1;486.42, 149.13, 1]';
 % PtD = [151.51, 136.34, 1;165.96, 141.96, 1;151.51, 146.46, 1;165.96, 149.83, 1]';
@@ -31,9 +31,11 @@ img3 = double(imread('Amst-1.jpg'))/255;
 
 PtO = [472.18, 135.74, 1;486.42, 139.97, 1;465.74, 247.23, 1;496.36, 247.92, 1]';
 PtD = [151.51, 136.34, 1;165.96, 141.96, 1;141.29, 250.84, 1;172.31, 248.33, 1]';
+% PtD = [1151.51, 236.34, 1;1165.96, 241.96, 1;1141.29, 350.84, 1;1172.31, 348.33, 1]'; % coodinate in im_fused
 % PtD = [651.51, 236.34, 1;665.96, 241.96, 1;641.29, 350.84, 1;672.31, 348.33, 1]'; % coodinate in im_fused
 H3 = homography2d(PtO,PtD);
-bbox1 = [-500 1000 -100 500];
+% bbox1 = [-500 1000 -100 500];
+bbox1 = [-1000 1000 -100 500];
 imc=vgg_warp_H(img3,H3,'linear',bbox1);
 imagesc(imc);
 im_final = max(imc, im_fused)
